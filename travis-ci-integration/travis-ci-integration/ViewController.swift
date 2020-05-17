c545b9b4c29c32105d8a84d47de7f7ed93fa2263
Anish <kode.iosdev@gmail.com>
2020-05-17 15:41:45 +0530

    project created

---
+++ travis-ci-integration/travis-ci-integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travis-ci-integration
+//
+//  Created by Anish on 5/17/20.
+//  Copyright © 2020 Anish Kodeboyina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ travis-ci-integration/travis-ci-integrationTests/travis_ci_integrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  travis_ci_integrationTests.swift
+//  travis-ci-integrationTests
+//
+//  Created by Anish on 5/17/20.
+//  Copyright © 2020 Anish Kodeboyina. All rights reserved.
+//
+
+import XCTest
+@testable import travis_ci_integration
+
+class travis_ci_integrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
