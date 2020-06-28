bbad97eff1a6b886254b5c0ea2a863a00e20093d
Anish <kode.iosdev@gmail.com>
2020-06-28 17:24:35 +0530

    firebase deployment integration and travis yml file modification

--- travis-ci-integration/travis-ci-integration/AppDelegate.swift
+++ travis-ci-integration/travis-ci-integration/AppDelegate.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -23,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        FirebaseApp.configure()
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
